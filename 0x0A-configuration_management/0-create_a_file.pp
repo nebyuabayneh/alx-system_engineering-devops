0ee2e7032b29df818a7831539804b26b24513d7e
Nebyu <nebyouabayneh@gmail.com>
2023-01-11 21:57:15 +0300

    Configuration

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file in /tmp using Puppet
 
-file { 'holberton':
+file { 'school':
   ensure  => present,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
