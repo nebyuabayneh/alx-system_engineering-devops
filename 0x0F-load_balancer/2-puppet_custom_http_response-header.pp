c43b591aa0d80a2826549bab0f79655c4f5809bf
Nebyu <nebyouabayneh@gmail.com>
2023-01-19 21:25:31 +0300

    Load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,69 +1,35 @@
-# Nginx web server setup and configuration
-
-exec { 'update-apt':
-
+# puppet manifest creating a custom HTTP header response
+exec { 'apt-get-update':
   command => '/usr/bin/apt-get update',
-
 }
-
-
 
 package { 'nginx':
-
   ensure  => installed,
-
-  require => Exec['update-apt'],
-
+  require => Exec['apt-get-update'],
 }
 
-
-
-file_line { 'Redirection':
-
+file_line { 'a':
   ensure  => 'present',
-
   path    => '/etc/nginx/sites-available/default',
-
   after   => 'listen 80 default_server;',
-
-  line    => 'rewrite ^/redirect_me https://sketchfab.com/bluepeno/models permanent;',
-
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
-
 }
 
-
-
-file_line { 'Custom-Header':
-
+file_line { 'b':
   ensure  => 'present',
-
   path    => '/etc/nginx/sites-available/default',
-
   after   => 'listen 80 default_server;',
-
   line    => 'add_header X-Served-By $hostname;',
-
   require => Package['nginx'],
-
 }
-
-
 
 file { '/var/www/html/index.html':
-
   content => 'Hello World!',
-
   require => Package['nginx'],
-
 }
 
-
-
 service { 'nginx':
-
   ensure  => running,
-
   require => Package['nginx'],
-
 }
